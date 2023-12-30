913182e034d5c7c2ba435f52f9c3f1572aaa308a
David Carrillo <davidcarrillocasais@gmail.com>
2023-12-30 19:55:46 +0100

    external library

---
+++ lib/lib1/src/prueba.py
@@ -0,0 +1,4 @@
+from user import User
+
+user = User(id=1, name='Kitty')
+print(user)
--- src/api/main.py
+++ src/api/main.py
@@ -1,8 +1,14 @@
 from src.repo1.hello import Hello
 from loguru import logger
 
+from user import User
+
 cosa: Hello = Hello()
 print(cosa.greet())
 
 logger.info('Logging!!!!')
+
+user = User(id=1, name='Hello')
+print(user)
+
 
